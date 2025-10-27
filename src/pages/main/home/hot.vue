2b4ee3908a2b606701b0ccccb4aa1973c926e0ea
wenxig <wenxinguo12@gmail.com>
2025-10-27 10:36:12 +0800

    fix: no level need hidden

--- src/pages/main/home/hot.vue
+++ src/pages/main/home/hot.vue
@@ -1,13 +1,14 @@
 <script setup lang='ts'>
-import { PluginConfigSearchHotPageTopButton, uni } from 'delta-comic-core'
-import LevelIcon from './levelIcon.vue';
+import { uni } from 'delta-comic-core'
+import LevelIcon from './levelIcon.vue'
+import { isEmpty } from 'es-toolkit/compat';
 
 
 </script>
 
 <template>
   <div class="w-full h-fit overflow-y-hidden overflow-x-auto scrollbar py-1 bg-(--van-background-2)">
-    <div class="h-full flex flex-col w-fit mx-4 items-center justify-around" v-for="btn of [<PluginConfigSearchHotPageTopButton>{
+    <div class="h-full flex flex-col w-fit mx-4 items-center justify-around" v-for="btn of [isEmpty(uni.content.ContentPage.levelboard) ? undefined : {
       bgColor: '#ff9212',
       name: '排行榜',
       icon: LevelIcon,
@@ -15,8 +16,9 @@ import LevelIcon from './levelIcon.vue';
         const first = uni.content.ContentPage.levelboard.keys().next().value!
         return $router.force.push(`/hot?plugin=${first}`)
       }
-    }, ...Array.from(uni.content.ContentPage.topButton.values()).flat()]">
-      <button class="size-12  rounded-full flex items-center justify-center" :style="{ backgroundColor: btn.bgColor }" @click="btn.onClick">
+    }, ...Array.from(uni.content.ContentPage.topButton.values()).flat()].filter(v => !!v)">
+      <button class="size-12  rounded-full flex items-center justify-center" :style="{ backgroundColor: btn.bgColor }"
+        @click="btn.onClick">
         <NIcon color="white" size="calc(var(--spacing) * 6.5)">
           <component :is="btn.icon" />
         </NIcon>
