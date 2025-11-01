c9901d00494430bc2cc6232b48d5f727e39c3552
w enxig <wenxinguo12@yeah.net>
2025-11-01 20:04:16 +0800

    fix: workflow

--- script/build.sh
+++ script/build.sh
@@ -12,14 +12,14 @@ else
 fi
 
 # build Android
-# cap sync
-# cap build android
+cap sync
+cap build android
 
-# # copy signed APK into dist and rename to app.apk
-# APK_SRC="./android/app/build/outputs/apk/release/app-release-signed.apk"
-# if [ -f "$APK_SRC" ]; then
-#   cp -f "$APK_SRC" dist/app.apk
-# else
-#   echo "APK not found at $APK_SRC" >&2
-#   exit 1
-# fi
+# copy signed APK into dist and rename to app.apk
+APK_SRC="./android/app/build/outputs/apk/release/app-release-signed.apk"
+if [ -f "$APK_SRC" ]; then
+  cp -f "$APK_SRC" dist/app.apk
+else
+  echo "APK not found at $APK_SRC" >&2
+  exit 1
+fi
