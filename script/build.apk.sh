5161b0a156f8e585ebf5502c036362cafba77918
wenxig <wenxinguo12@gmail.com>
2025-12-07 08:58:41 +0800

    sign

--- script/build.apk.sh
+++ script/build.apk.sh
@@ -6,8 +6,19 @@ cap build android
 
 # copy signed APK into dist and rename to app.apk
 APK_SRC="./android/app/build/outputs/apk/release/app-release-signed.apk"
+
 if [ -f "$APK_SRC" ]; then
-  cp -f "$APK_SRC" dist/app.apk
+  apksigner sign \
+    --ks ./android/keystore.jks \
+    --ks-key-alias key0 \
+    --ks-pass 123456 \
+    --key-pass 123456 \
+    --v1-signing-enabled true \
+    --v2-signing-enabled true \
+    --v3-signing-enabled true \
+    --out app-release.apk \
+    "$APK_SRC"
+  cp -f "./android/app/build/outputs/apk/release/app-release.apk" dist/app.apk
 else
   echo "APK not found at $APK_SRC" >&2
   exit 1
