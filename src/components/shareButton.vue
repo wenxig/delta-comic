dbf9aacc0929376cab8ee00b425d1b2a033329ed
wenxig <wenxinguo12@gmail.com>
2025-12-10 00:19:11 +0800

    feat: change app icon

--- src/components/shareButton.vue
+++ src/components/shareButton.vue
@@ -1,14 +1,19 @@
 <script setup lang='ts'>
 import { ShareSharp } from '@vicons/material'
 import { Comp } from 'delta-comic-core'
+import type { ShareSheetOption } from 'vant'
+import { computed } from 'vue'
 import { shallowRef } from 'vue'
 
 const showShare = shallowRef(false)
+const options = computed<ShareSheetOption[]>(() => {
+
+})
 </script>
 
 <template>
   <Comp.ToggleIcon padding size="27px" :icon="ShareSharp" dis-changed>
     分享
   </Comp.ToggleIcon>
-  <VanShareSheet v-model:show="showShare" title="立即分享给好友" :options="options" @select="onSelect" />
+  <VanShareSheet v-model:show="showShare" title="立即分享给好友" :options="options" />
 </template>
