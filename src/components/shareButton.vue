7afb44893af33408915385f4fcaba18a6283104a
wenxig <wenxinguo12@gmail.com>
2025-12-10 00:34:46 +0800

    [skip ci] chore: share button prototype

--- src/components/shareButton.vue
+++ src/components/shareButton.vue
@@ -1,19 +1,20 @@
 <script setup lang='ts'>
 import { ShareSharp } from '@vicons/material'
-import { Comp } from 'delta-comic-core'
-import type { ShareSheetOption } from 'vant'
+import { Comp, uni } from 'delta-comic-core'
 import { computed } from 'vue'
 import { shallowRef } from 'vue'
-
+const $props = defineProps<{
+  page: uni.content.ContentPage
+}>()
 const showShare = shallowRef(false)
-const options = computed<ShareSheetOption[]>(() => {
-
-})
+const options = computed(() => Array.from(uni.content.ContentPage.share.entries().filter(v => v[1].filter($props.page))))
 </script>
 
 <template>
   <Comp.ToggleIcon padding size="27px" :icon="ShareSharp" dis-changed>
     分享
   </Comp.ToggleIcon>
-  <VanShareSheet v-model:show="showShare" title="立即分享给好友" :options="options" />
+  <Comp.Popup v-model:show="showShare" round position="bottom" class="h-30">
+    <VanButton block size="large" class="!w-full">取消</VanButton>
+  </Comp.Popup>
 </template>
