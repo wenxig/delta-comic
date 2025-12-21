1c3f879fb44061cd516f57f3491156715386a8d9
wenxig <wenxinguo12@gmail.com>
2025-12-22 00:01:45 +0800

    feat: resource define

--- src/plugin/utils.ts
+++ src/plugin/utils.ts
@@ -1,4 +1,3 @@
-import axios from "axios"
 import { Comp, Utils, type PluginConfig, type PluginConfigAuth, type PluginConfigAuthMethod } from "delta-comic-core"
 import { isEmpty, sortBy } from "es-toolkit/compat"
 import { delay } from "motion-v"
@@ -8,8 +7,17 @@ import { h, markRaw, ref } from "vue"
 import { defineComponent } from "vue"
 import { useAppStore } from "@/stores/app"
 
-export const testApi = async (cfg: NonNullable<PluginConfig['api']>[string]): Promise<[url: string, time: number | false]> => {
+export const testApi = async (cfg: NonNullable<PluginConfig['api']>[string]) => {
   const forks = await cfg.forks()
+  return await test(forks, cfg.test)
+}
+
+export const testResourceApi = (cfg: NonNullable<NonNullable<PluginConfig['resource']>['types']>[number]) => {
+  const forks = cfg.urls
+  return test(forks, cfg.test)
+}
+
+const test = async (forks: string[], test: (url: string, signal: AbortSignal) => PromiseLike<any>) => {
   if (isEmpty(forks)) throw new Error('[plugin test] no fork found')
   const record: [url: string, result: false | number][] = []
   const abortController = new AbortController()
@@ -19,68 +27,24 @@ export const testApi = async (cfg: NonNullable<PluginConfig['api']>[string]): Pr
       const stopTimeout = delay(() => {
         abortController.abort()
       }, 10000)
-      await cfg.test(fork, abortController.signal)
+      await test(fork, abortController.signal)
       stopTimeout()
       const end = Date.now()
       const time = end - begin
       record.push([fork, time])
-      console.log(`[plugin test] api url ${fork} connected time ${time}ms`)
+      console.log(`[plugin test] resource url ${fork} connected time ${time}ms`)
       abortController.abort()
     } catch (error) {
       record.push([fork, false])
-      console.log(`[plugin test] api url ${fork} can not connected`)
+      console.log(`[plugin test] resource url ${fork} can not connected`)
     }
   }))
   const result = sortBy(record.filter(v => v[1] != false), v => v[1])[0]
-  console.log(`[plugin test] api test done`, result)
+  console.log(`[plugin test] resource test done`, result)
   if (!result) {
-    return ['', false]
+    return ['', false] as [string, false]
   }
   return result
-}
-
-export const testImageApi = async (cfg: NonNullable<PluginConfig['image']>): Promise<Record<string, [url: string, time: number | false]>> => {
-  const api: Record<string, [url: string, time: number | false]> = {}
-  const namespaces = Object.keys(cfg.forks)
-  console.log(`[plugin test] image url`, cfg)
-  const results = await Promise.all(
-    namespaces.map<Promise<[url: string, result: number | false]>>(async namespace => {
-      const forks = cfg.forks[namespace]
-      if (isEmpty(forks)) throw new Error('[plugin testImageApi] not found any forks')
-      const record: [url: string, result: false | number][] = []
-      const abortController = new AbortController()
-      await Promise.all(forks.map(async fork => {
-        try {
-          const begin = Date.now()
-          const stopTimeout = delay(() => {
-            abortController.abort()
-          }, 10000)
-          await axios.get(`${fork}/${cfg.test}?random=${Math.random()}`, {
-            signal: abortController.signal
-          })
-          stopTimeout()
-          const end = Date.now()
-          const time = end - begin
-          record.push([fork, time])
-          console.log(`[plugin test] image url ${fork} connected time ${time}ms`)
-          abortController.abort()
-        } catch (error) {
-          record.push([fork, false])
-          console.log(`[plugin test] image url ${fork} can not connected`)
-        }
-      }))
-      const result = sortBy(record.filter(v => v[1] != false), v => v[1])[0]
-      console.log(`[plugin test] image test done`, result)
-      if (!result) {
-        return ['', false]
-      }
-      return result
-    })
-  )
-  namespaces.forEach((namespace, i) => {
-    api[namespace] = results[i]
-  })
-  return api
 }
 
 const authPopupMutex = new Mutex
